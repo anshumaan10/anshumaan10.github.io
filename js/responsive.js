aee832eae1a5f0c071b74b14e732a3f78fa2afa6
ANSHUMAAN SINGH RAWAT <55029643+anshumaan10@users.noreply.github.com>
2020-07-22 19:49:56 +0530

    Add files via upload

---
+++ js/responsive.js
@@ -0,0 +1,10 @@
+ <script>
+    function responsive_menu(){
+        var e= document.getElementsById('menu_small');
+        if(e.className==='toprightmenu'){
+            e.className+="responsive";
+
+        }else{
+            e.className='toprightmenu';
+        }
+        }</script>
